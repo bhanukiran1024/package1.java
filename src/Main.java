d4755bcad528f6b321886c434a6bdb48e34f4de0
bhanukiran1024 <31910740+bhanukiran1024@users.noreply.github.com>
2017-09-13 12:46:29 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student s1=new Student(1,"mahi gangi","00-00-0000",85.6);
+		Student s2=new Student(1,"pavan penu","00-00-0000",75.6);
+		Student s3=new Student(1,"bhanu gunda","00-00-0000",80.6);
+	}
+
+}
